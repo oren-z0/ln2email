16920adf7ab434811b5d38958f1d6392bf0ac050
Oren <orenz0@protonmail.com>
2023-08-24 23:15:50 +0300

    remove mongo client promise from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
 import { GetStaticProps } from 'next';
 import Home from '@/components/home';
-import clientPromise from '@/lib/mongodb';
 
 export default function Index() {
   return <Home />;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  try {
-    await clientPromise;
-  } catch (e: any) {
-    console.error(e);
-    throw new Error('Connection limit reached. Please try again later.');
-  }
-
   return {
     props: {},
     revalidate: 10
